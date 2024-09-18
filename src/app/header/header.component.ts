e31ceb6d220c8309a1cfdf8c3a6a0a64c32e45f9
Dante Mogrim <dante.mogrim@odevo.com>
2024-09-18 13:09:17 +0200

    build(assets): fill app with real content
    No more placeholders.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -15,6 +15,14 @@ import { Component } from '@angular/core';
       <h1 class="font-silk text-4xl px-2">Crib Your Enthusiasm</h1>
     </div>
     <h2 class="text-xl italic">Iconic houses have never looked this good.</h2>
+    <section class="p-3 bg-blue-300 rounded-xl my-2  text-blue-900">
+      <h3 class="text-lg">
+        Are you able to guess which house belongs to which movie/series?
+      </h3>
+      <h4 class="font-bold">
+        Tap on one of the listings to reveal where to house is known from.
+      </h4>
+    </section>
   </header>`,
 })
 export class HeaderComponent {}
